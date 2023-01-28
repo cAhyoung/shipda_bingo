b1555f14ac05b101cea8c55ee440c33caa9c3a98
Ahyoung Cho <joayoung03@gmail.com>
2023-01-28 21:31:10 +0900

    1. mypage: 상단에 닉네임과 레벨 정보, 각 버튼 누를 시 액티비티 또는 다이얼로그 뜸 2. ChanbeNick: 닉네임 수정하는 액티비티 3. ChangePw: 비밀번호 수정하는 액티비티, 기존 비밀번호 확인 후 비밀번호 조건 맞춰서 수정 가능하도록 함 4. LogOut: dialog를 이용해 login 액티비티로 전환함 5. Delete: dialog를 이용해 계정 탈퇴 진행, login액티비티로 전환함

--- app/src/main/java/com/example/happypig/mypage/ChangeNick.kt
+++ app/src/main/java/com/example/happypig/mypage/ChangeNick.kt
@@ -1,12 +1,49 @@
 package com.example.happypig.mypage
 
+import android.content.Intent
+import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import com.example.happypig.DBManager
 import com.example.happypig.R
+import com.example.happypig.home.HomeActivity
 
 class ChangeNick : AppCompatActivity() {
+
+    lateinit var changeNick : EditText
+    lateinit var acceptBtn : Button
+    lateinit var cursor : Cursor
+    private val id by lazy {
+        intent.getStringExtra("id").toString()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_nick)
+
+        val myHelper = DBManager(this, "guruDB", null, 1)
+        var db = myHelper.writableDatabase
+
+        // 닉네임 변경
+        changeNick = findViewById(R.id.changeNickEt)
+        acceptBtn = findViewById(R.id.acceptBtn)
+
+
+        acceptBtn.setOnClickListener{
+            db.execSQL("UPDATE personnel SET nickname = '" + changeNick.text + "' WHERE id = '" + id + "';")
+            db.close()
+            Toast.makeText(this, "닉네임이 변경되었습니다", Toast.LENGTH_SHORT).show()
+        }
+
+
+        // Change Nickname용 toolbar
+        val toolbarChangeNick = findViewById<Toolbar>(R.id.toolbarChangeNick)
+        setSupportActionBar(toolbarChangeNick)
+
     }
 }
